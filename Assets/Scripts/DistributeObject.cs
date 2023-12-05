eab52f28e840915fff15dfa746d5ef221dbf442a
Mathieu LAURENCOT <malaurenco@local.isima.fr>
2023-12-05 08:49:34 +0100

    amelio objet des distrib

--- Assets/Scripts/DistributeObject.cs
+++ Assets/Scripts/DistributeObject.cs
@@ -12,7 +12,15 @@ public class DistributeObject : MonoBehaviour
 
     void CreatePrefab()
     {
-        _uniqueId = Instantiate(_thePrefab, GetComponent<Transform>().position+Vector3.down*1, Quaternion.identity).GetInstanceID();
+        GameObject justCreated = Instantiate(_thePrefab, GetComponent<Transform>().position+Vector3.down*1, Quaternion.identity);
+        justCreated.GetComponent<MeshCollider>().convex = true;
+        justCreated.tag = "Grabbable";
+        justCreated.AddComponent<TargetSelect>();
+        justCreated.AddComponent<Outline>();
+        Rigidbody rb = justCreated.AddComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic; //Si probleme changer ici
+        rb.mass = 1;
+        _uniqueId = justCreated.GetInstanceID();
     }
 
 
