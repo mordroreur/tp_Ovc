5e7a13150dfcd9b157fa72aed2a42a590caaafd2
mordroreur <mlaurencot@gmail.com>
2023-12-16 18:04:07 +0100

    add menu

--- Assets/Scripts/EyeMouvment.cs
+++ Assets/Scripts/EyeMouvment.cs
@@ -8,30 +8,62 @@ public class WalkMovement : MonoBehaviour
     public float _mouseSensitivity = 400.0f;
 
     public Transform _orientation;
+    public GameObject _escapeMenu;
+
+    public bool _isActivetedOnStart = true;
 
     private float _yRot;
     private float _xRot;
+    private bool _isSelected = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        if (_isActivetedOnStart)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            _escapeMenu.SetActive(false);
+            _isSelected = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * _mouseSensitivity;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _mouseSensitivity;
+        if (_isSelected)
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * _mouseSensitivity;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _mouseSensitivity;
 
-        _yRot += mouseX;
-        _xRot -= mouseY;
+            _yRot += mouseX;
+            _xRot -= mouseY;
 
-        _xRot = Mathf.Clamp(_xRot, -90f, 90f);
+            _xRot = Mathf.Clamp(_xRot, -90f, 90f);
 
-        GetComponent<Transform>().rotation = Quaternion.Euler(_xRot, _yRot, 0);
-        _orientation.rotation = Quaternion.Euler(0, _yRot, 0);
+            GetComponent<Transform>().rotation = Quaternion.Euler(_xRot, _yRot, 0);
+            _orientation.rotation = Quaternion.Euler(0, _yRot, 0);
+
+            if (Input.GetKeyDown("escape"))
+            {
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                _escapeMenu.SetActive(true);
+                _isSelected = false;
+            }
+        }
+        
+
+
+    }
+
+    public void enterGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _escapeMenu.SetActive(false);
+        _isSelected = true;
     }
 
 }
